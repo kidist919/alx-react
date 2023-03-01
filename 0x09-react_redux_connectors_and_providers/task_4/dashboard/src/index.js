2fbd8179505030a936abfd66b2b4a8eb54810f56
kidist919 <99682094+kidist919@users.noreply.github.com>
2023-03-01 03:14:59 -0800

    Update index.js

--- 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App/App';
-import Notifications from "./Notifications/Notifications";
+import React from "react";
+import ReactDOM from "react-dom";
+import { creatStore, Provider, applyMiddleware, compose, combineReducers} from "react-redux";
+import thunk from 'redux-thunk'
+import App from "./App/App";
+import rootReducer, { initialState } from './reducers/rootReducer';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = creatStore(combineReducers(rootReducer), initialState, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
