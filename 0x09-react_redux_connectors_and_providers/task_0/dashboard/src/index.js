26ebeba9b00b02ca8647df15ae0372a8a32ca92b
kidist919 <99682094+kidist919@users.noreply.github.com>
2023-03-01 02:27:03 -0800

    Update index.js

--- 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App/App';
-import Notifications from "./Notifications/Notifications";
+import React from "react";
+import ReactDOM from "react-dom";
+import { creatStore, Provider } from "react-redux";
+import App from "./App/App";
+import { uiReducer } from "./reducers/uiReducer";
+
+const store = creatStore(uiReducer)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
